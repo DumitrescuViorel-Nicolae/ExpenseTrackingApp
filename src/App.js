10519c34fefb045db1bd2ec104c801c27f039368
Dumitrescu Viorel-Nicolae <dterminator2020@gmail.com>
2022-07-19 00:18:34 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/New Expenses/NewExpense";
+
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(),
+    },
+
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(),
+    },
+
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(),
+    },
+
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(),
+    },
+  ];
+
+  return (
+    <div>
+      <NewExpense />
+      <Expenses expenses={expenses} />
+    </div>
+  );
+}
+
+export default App;
